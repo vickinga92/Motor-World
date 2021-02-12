c96809a87aaf4579ac4ddb48f4b383aab5770f28
vickinga92 <m.v-tauro@hotmail.com>
2021-02-12 22:11:11 +0100

    traer cambios

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,6 @@
 
 const expressApp = require('./app')
 
-const port = process.env.PORT || 8082
+const port = /* process.env.PORT || */ 8082
 
 expressApp.listen(port, () => console.info(`El servidor está corriendo en http://localhost:${port}`))
