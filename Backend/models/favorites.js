0f9447e6b94a3e5c2ae2fb67ab2552a50d4884a4
bokejor <jorgecarmarquez@gmail.com>
2021-02-17 15:24:36 +0100

    Se resuelve la gestión de favoritos, se optimiza algo más los filtros

--- Backend/models/favorites.js
+++ Backend/models/favorites.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose')
 const FavoritesSchema = require('./schemas/favorites')
 
-// FavoritesSchema.methods.toJSON = function() {
-//     const { _id, userId, ...motoId, __v } = this.toObject();
-//     return motoId;
-//   }
-
 let Favorites = mongoose.model('Favorites', FavoritesSchema);
 
 module.exports = Favorites
--- Backend/routes/myFavorites.js
+++ Backend/routes/myFavorites.js
@@ -1,59 +1,22 @@
 const mongoose = require('mongoose')
 const express = require('express')
-const { json } = require('express')
 const router = express.Router()
 const Favorites = require('../models/favorites')
-const User = require('../models/users')
 const mustAuth = require('../middlewares/mustAuth')
-
 
 router.route('/favorites')
   .get(mustAuth(), async (req, res) => {
-    filters = { userId: req.user._id }
-    const respuesta = [];
-    const  favoritesMotos = await Favorites.find(filters)    
-    .populate({ path: 'motoId', model: 'motos'})
-
-    res.json(favoritesMotos.map(m => m.motoId))
-
-  })
-  .post(mustAuth(), async (req, res) => {
     
-    try {
-      const motoId =  req.body.motoId;
-      const userId =  req.user._id;
-      
-      const existeMoto = await Favorites.findOne({ motoId });
-      if ( existeMoto ) {
-        return res.json(existeMoto);
-      }
-
-      let favoriteInMongo = await new Favorites({userId, motoId}).save()
-
-      res.json(favoriteInMongo);
-
-    } catch (e) {
-      res.status(500).json({ error: e.message })
-    }
-  })
-
- router.route('/favorites/filterDesc')
-  .get(mustAuth(), async (req, res) => {
-
     filters = { userId: req.user._id }
-    filterPriceDesc = { price: -1 }
-    let priceDesc = await Favorites.find(filters).sort(filterPriceDesc).exec()
 
-    res.json(priceDesc)
-  })
-router.route('/favorites/filterAsc')
-  .get(mustAuth(), async (req, res) => {
-    filters = { userId: req.user._id }
-    let filterPriceAsc = { price: 1 }
-    let priceAsc = await Favorites.find(filters).sort(filterPriceAsc).exec()
+    const favorites = await Favorites.find(filters)     
+    .populate({ path: 'motoId', model: 'motos'})
+    .populate({ path: 'postId', model: 'postModel'})
+ 
+    res.json(favorites)
 
-    res.json(priceAsc)
   })
+
 router.route('/favorites/:id')
   .get(mustAuth(), async (req, res) => {
 
@@ -67,21 +30,46 @@ router.route('/favorites/:id')
     }
 
     res.json(foundItem)
-  }) 
-  .delete(mustAuth(), async (req, res) => {
+  })
+  .put(mustAuth(), async (req, res) => {
+    
+    try {
 
-    let searchId = req.params.id
+      let params; 
+      const favoriteId =  req.params.id;     
+      const userId =  req.user._id;      
+      const isMoto = req.body.isMoto;
 
+      isMoto ? params = {$addToSet: {motoId : favoriteId}} : params = {$addToSet: {postId : favoriteId}}     
+      
+      let favoriteInMongo = await Favorites.findOneAndUpdate({userId: userId},params,
+        {new:true, upsert : true })
 
-    let foundItem = await Favorites.findOneAndDelete({ _id: searchId }).exec()
+      res.json(favoriteInMongo);
 
-    if (!foundItem) {
-      res.status(404).json({ 'message': 'El elemento que intentas eliminar no existe' })
-      return
+    } catch (e) {
+      res.status(500).json({ error: e.message })
     }
+  })
+  .delete(mustAuth(), async (req, res) => {
+    try {
+     
+      const favoriteId = req.params.id;      
+      const userId =  req.user._id; 
 
-    res.status(204).json({ 'message': 'El elemento se ha eliminado correctamente' })
+      let foundItem = await Favorites.findOneAndUpdate(
+        {userId: userId},{$pull: {motoId :{ $in: favoriteId }, postId :{ $in: favoriteId }}})
+
+      if (!foundItem) {
+        res.status(404).json({ 'message': 'El elemento que intentas eliminar no existe' })
+        return
+      }
+
+      res.status(204).json({ 'message': 'El elemento se ha eliminado correctamente' })
+
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
   });
-
 
 module.exports = router
