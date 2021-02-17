d0bc65966e71a7aec0e4693ce9f0c3e23b0e16c8
bokejor <jorgecarmarquez@gmail.com>
2021-02-17 15:27:04 +0100

    Eliminado modelo FavoritePost

--- Backend/models/favoritesPost.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const PostSchema = require('./schemas/post')
-
-let FavoritesModel = mongoose.model('favoritesPost', PostSchema);
-
-module.exports = FavoritesModel
